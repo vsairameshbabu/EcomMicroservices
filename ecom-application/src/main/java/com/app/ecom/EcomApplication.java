5a11aea3a129c38a69bf5ea864d340bb4dca4543
vsairameshbabu <vsairameshbabu27@gmail.com>
2025-07-27 22:04:48 +0530

    Initial Commit

--- ecom-application/src/main/java/com/app/ecom/EcomApplication.java
+++ ecom-application/src/main/java/com/app/ecom/EcomApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcomApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EcomApplication.class, args);
 	}
 
