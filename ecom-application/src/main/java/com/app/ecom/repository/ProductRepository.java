5340c54dc2c65359c638c8e5f16b0a0d400ce7c6
vsairameshbabu <vsairameshbabu27@gmail.com>
2025-08-02 12:47:15 +0530

    OrderPlaced

--- ecom-application/src/main/java/com/app/ecom/repository/ProductRepository.java
+++ ecom-application/src/main/java/com/app/ecom/repository/ProductRepository.java
@@ -1,12 +1,10 @@
 package com.app.ecom.repository;
 
-import com.app.ecom.dto.ProductResponse;
 import com.app.ecom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Collection;
 import java.util.List;
 
 @Repository
@@ -14,5 +12,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByActiveTrue();
 
-    List<Product> searchProducts(String keyword);
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(@Param("keyword") String keyword);
 }
